85e09ef6fe54580e18d9e6b514247af0ecf0c86e
isabel1410 <97336074+isabel1410@users.noreply.github.com>
2023-05-09 10:15:59 +0200

    Update and rename Settings.cs to SettingsUI.cs

--- SettingsUI.cs
+++ SettingsUI.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Settings : MonoBehaviour
+public class SettingsUI : MonoBehaviour
 {
     [Header("UI elements")]
     [SerializeField] private Slider _width;
